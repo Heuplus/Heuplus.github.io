77c1995c3c72ade656c4207e58d7bfc98449b421
Mustafa BOLEKEN <mustafaboleken@yahoo.com>
2025-04-20 16:47:30 +0300

    updaTE COLOR

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,14 +7,14 @@ const Home: NextPage = () => {
   return (
     <div className='h-full w-full '>
       <div className="mt-20 absolute inset-0 bg-[url(/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
-      <div className="w-full h-40 absolute bg-light-purple dark:purple dark:bg-black">
-        <div className="sm:ml-0 ml-5 mr-0 mt-3 md:pl-80 md:pr-80 sm:w-full h-full bg-purple-500 dark:bg-black">
-          <Image src="/vercel.svg" width={150} height={100} className="w-40 h-16" alt="Tailwind Play" />
+      <div className="w-full h-40 absolute" style={{ backgroundColor: 'rgb(214, 224, 232)' }}>
+        <div className="sm:ml-0 ml-5 mr-0 mt-3 md:pl-80 md:pr-80 sm:w-full h-full" style={{ backgroundColor: 'rgb(214, 224, 232)' }}>
+          <Image src="/heuplus.svg" width={150} height={100} className="w-40 h-16" alt="Tailwind Play" />
         </div>
       </div>
-      <div className='mt-20 w-full absolute overflow-scroll	'>
+      <div className='mt-20 w-full absolute overflow-scroll'>
         <ServicesSection />
-      </div >
+      </div>
     </div>
   )
 }
