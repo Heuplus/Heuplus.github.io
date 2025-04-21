4e764faa76c0101eadb529c312f5e4be9cdc4e9a
Mustafa BOLEKEN <mustafaboleken@yahoo.com>
2025-04-21 19:01:58 +0300

    Update title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
 			<Head>
-				<title>System Status</title>
+				<title>Heuplus Status</title>
 			</Head>
 			<Component {...pageProps} />
 		</>
